0a2dc03543f632cd5e7b0f96152281ae99ab288b
danielebilade-lgtm <danielebilade@gmail.com>
2026-02-13 11:13:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
